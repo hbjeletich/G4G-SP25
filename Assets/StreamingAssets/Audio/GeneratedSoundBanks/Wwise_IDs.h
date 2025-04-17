5deb853b9b2371f6a717b66eedaa44bc50f83af2
DanZun <34405370+pandita7@users.noreply.github.com>
2025-04-16 22:52:45 -0500

    Wwise Path Fix, UI Sounds
    Fixed Unity errors pertaining to wwise soundbank settings

---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,51 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID GOLF_ENVIRONMENT = 4208230819U;
+        static const AkUniqueID GOLF_ENVIRONMENT_IMPACT = 1621041688U;
+        static const AkUniqueID GOLF_IMPACT = 1183497998U;
+        static const AkUniqueID GOLF_SCORE = 3905678478U;
+        static const AkUniqueID GOLF_START = 1594576204U;
+        static const AkUniqueID GOLF_SWING = 3110887642U;
+        static const AkUniqueID GOLF_TRANSITION = 4261217065U;
+        static const AkUniqueID GOLF_WIND_UP = 271725226U;
+        static const AkUniqueID WIND_NOISE = 650903076U;
+    } // namespace EVENTS
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID WIND_VOLUME = 4244623852U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN_GOLF = 899873513U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
