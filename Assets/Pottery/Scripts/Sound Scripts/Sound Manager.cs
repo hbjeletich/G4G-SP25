c7c49ad14f58c9d618ec09e3fd4b6339227ec32d
DavidZ-UT <dzambrano2004@gmail.com>
2025-12-09 14:30:05 -0600

    Update Sound Manager.cs

--- Assets/Pottery/Scripts/Sound Scripts/Sound Manager.cs
+++ Assets/Pottery/Scripts/Sound Scripts/Sound Manager.cs
@@ -7,6 +7,7 @@ public class SoundManager : MonoBehaviour
     [SerializeField] AudioSource musicSource;
     [SerializeField] AudioSource SFXSource;
 
+    //List of audio for when the script is attatched
     [Header("--------- Audio Clip ----------")]
     public AudioClip menu;
     public AudioClip button;
@@ -21,16 +22,19 @@ public class SoundManager : MonoBehaviour
         musicSource.Play();
     }
 
+    //Below is the function for playing a sound effect, which can be attatched to buttons
     public void PlaySFX(AudioClip clip)
     {
         SFXSource.PlayOneShot(clip);
     }
 
+    //The following is a function to fade the music out and can be added to the buttons to transition audios
     public void FadeOutMusic(float duration = 1f)
     {
         StartCoroutine(FadeOutMusicCoroutine(duration));
     }
 
+    //The following is the mechanism behind changing the volume in the FadeOutMusic function
     private IEnumerator FadeOutMusicCoroutine(float duration)
     {
         float startVol = musicSource.volume;
